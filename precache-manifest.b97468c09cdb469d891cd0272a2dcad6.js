69a276e552df038fddfe6197451ecd197d2f0e7b
Lakshya Kumar <lakshyak983@gmail.com>
2022-01-20 01:48:15 +0530

    Updates

---
+++ precache-manifest.b97468c09cdb469d891cd0272a2dcad6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27475764760b97faf2d184936be374a1",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "cd08d2c868be4ac038db",
+    "url": "/tic-tac-toe/static/css/main.e876769f.chunk.css"
+  },
+  {
+    "revision": "023fe1b411c6f4863612",
+    "url": "/tic-tac-toe/static/js/2.4fd0d418.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.4fd0d418.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd08d2c868be4ac038db",
+    "url": "/tic-tac-toe/static/js/main.b41f5537.chunk.js"
+  },
+  {
+    "revision": "5c4189ecdc0b3d19f71c",
+    "url": "/tic-tac-toe/static/js/runtime-main.af210e20.js"
+  }
+]);
